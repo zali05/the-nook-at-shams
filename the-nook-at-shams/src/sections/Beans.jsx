4b165e514117a353818de94322c27cc19edec566
Zain Ali <zain.s.ali03@gmail.com>
2026-03-07 16:06:09 -0800

    added the gallery section

--- the-nook-at-shams/src/sections/Beans.jsx
+++ the-nook-at-shams/src/sections/Beans.jsx
@@ -1,26 +1,34 @@
 import v60Video from '../assets/videos/v60.mp4';
+import {motion} from 'framer-motion';
 
 export default function Beans() {
     return(
         <section className="min-h-screen flex items-center  bg-[#111] text-white px-6">
             <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-12 items-center">
                 
-                <div>
+                <motion.div
+                initial={{ opacity: 0, x: -50 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1 }}
+                viewport={{once:true}}>
                 <h2 className="text-4xl md:text-6xl font-semibold leading-tight">Our Coffee</h2>
                 <p className="mt-6 text-lg text-gray-300">
                     Discover the rich flavors of our carefully selected coffee beans. Our mission is not just to present you with a cup of good coffee, but rather to send you on a journey of exploration through the tradition and culture of coffee
                 </p>
-                </div>
+                </motion.div>
 
                 <div className="relative w-full max-w-md mx-auto overflow-hidden rounded-3xl shadow-2xl">
                     <a href= "https://www.instagram.com/thenookatshams/?hl=en" target="_blank" rel="noopener noreferrer" className="block relative w-full max-w-md mx-auto overflow-hidden rounded-3xl shadow-2xl" >
-                    <video
+                    <motion.video
                     src={v60Video}
                     autoPlay
                     loop
                     muted
                     playsInline
-                    className="w-full h-full object-cover"
+                    initial = {{ scale:1}}
+                    whileInView={{scale: 1.05}}
+                    transition = {{ duration: 3, ease: "easeOut"}}
+                    className="w-full h-full"
                     />
 
                     <div className="absolute inset-0 bg-black/20 opacity-0 hover:opacity-100 transition flex items-center justify-center">
--- the-nook-at-shams/src/sections/Gallery.jsx
+++ the-nook-at-shams/src/sections/Gallery.jsx
@@ -1,7 +1,48 @@
+import { motion} from 'framer-motion';
+import v60Video from '../assets/videos/v60.mp4';
+import cookie from '../assets/videos/cookie.mp4';
+import latte from '../assets/videos/latte.mp4';
+
+
 export default function Gallery() {
+    const videos = [v60Video, cookie, latte];
     return(
-        <section>
-            <h1>Hero Section</h1>
+        <section className="bg-[#0a0a0a] text-white py-32 px-6">
+            <motion.div
+            initial = {{ opacity: 1, y: 40}}
+            whileInView={{opacity: 1, y:0}}
+            transition={{ duration: 0.8, ease: "easeOut"}}
+            viewport={{ once: true }}
+            className="text-center mb-16"
+            >
+                <h2 className="text-4xl md:text-6xl font-semibold">Feel the vibe</h2>
+                <p className="mt-4  text-gray-400">A glimpse into the world of The Nook at Shams</p>
+            </motion.div>
+
+            <div className="grid md:grid-cols-3 gap-8 max-w-6xl mx-auto">
+                {videos.map((video, i) =>(
+                    <motion.div
+                    key={i}
+                    initial={{ opacity: 0, y: 60 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.8, delay: i * 0.3, ease: "easeOut" }}
+                    viewport={{ once: true }}
+                    className="flex justify-center"
+                    >
+                     <div className="overflow-hidden rounded-3xl shadow-xl max-w-xs">
+                        <video
+                        src={video}
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
+                        className="w-full h-auto"
+                        />
+                    </div>
+                    </motion.div>
+                ))}
+                
+            </div>
         </section>
     );
 }
